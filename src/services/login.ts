8c63f4c895e0cba8f50e7aee56723483816907b4
StarkOne <stark.pro40@gmail.com>
2020-05-31 19:28:20 +0700

    headless=true

--- src/services/login.ts
+++ src/services/login.ts
@@ -2,7 +2,7 @@ const puppeteer = require("puppeteer");
 import { Browser, Page } from "puppeteer";
 
 const login = async ({ LOGIN, PASSWORD }) => {
-  const browser: Browser = await puppeteer.launch({ headless: false });
+  const browser: Browser = await puppeteer.launch({ headless: true });
   const page: Page = await browser.newPage();
   await page.setViewport({ width: 1200, height: 2500 });
   await page.goto("http://ytr.jilfond.org:8943/", {
